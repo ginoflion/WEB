82ff8c666a09db5acdb55852627bbf921a507a13
Goncalo Silva <goncalosilva120899@gmail.com>
2023-11-09 16:01:37 +0000

    Estados done + organização

--- things.js
+++ things.js
@@ -2,13 +2,14 @@ var temporizador;
 var tempoInicial = 30;
 var tempoAtual = tempoInicial;
 var emPausa = true; 
+
+
 //#region Temporizador
 function atualizarTemporizador() {
     var minutos = Math.floor(tempoAtual / 60);
     var segundos = tempoAtual % 60;
     segundos = segundos < 10 ? '0' + segundos : segundos;
     document.getElementById('temporizador').innerHTML = minutos + ':' + segundos;
-
     if (tempoAtual === 0) {
         clearInterval(temporizador);
         alert('Tempo esgotado!');
@@ -47,9 +48,9 @@ const larguraCelula = 130;
 const alturaCelula = 130;
 const numeroColunas = 3;
 
-
+//#region Movimentacao
 function moverPersonagem(e) {
-    if (!emPausa) { // Verifica se o jogo não está em estado de pausa
+    if (!emPausa) { 
         const celulaAtual = document.querySelector('.personagem').parentElement;
         let novaCelula;
 
@@ -80,11 +81,13 @@ function moverPersonagem(e) {
 
 window.addEventListener('keydown', moverPersonagem, 300);
 
+//#endregion
 const personalizacaoForm = document.getElementById('personalizacaoForm');
 const olhoEsquerdo = document.querySelector('.olho.E');
 const olhoDireito = document.querySelector('.olho.D');
 const nomeJogador = document.getElementById('nome-jogador');
 
+//#region Personalizaçao
 personalizacaoForm.addEventListener('submit', function(event) {
     event.preventDefault(); 
 
@@ -98,12 +101,13 @@ personalizacaoForm.addEventListener('submit', function(event) {
     
     nomeJogador.textContent = nomePersonagem;
 });
-
+//#endregion
 
 const celulas = document.querySelectorAll('.tabuleiro td');
 let quadrados = [];
 
 
+//#region SpawnAleatório
 function spawnQuadrados() {
     quadrados.forEach(quadrado => {
         quadrado.parentNode.removeChild(quadrado);
@@ -129,14 +133,18 @@ function spawnQuadrados() {
         quadrados.push(quadrado);
     });
 }
+//#endregion
 
-let pontuacao = 0; // Inicializa a pontuação
+let pontuacao = 0; 
 
+//#region Pontuação
 function atualizarPontuacao(pontos) {
     pontuacao += pontos; // Aumenta a pontuação pelos pontos fornecidos
     document.getElementById('pontuacao-jogador').textContent = pontuacao; // Atualiza a exibição da pontuação no HTML
 }
+//#endregion
 
+//#region Colisões
 function verificarColisao() {
     const celulaPersonagem = personagem.parentElement;
     let colidiu = false;
@@ -145,60 +153,70 @@ function verificarColisao() {
         const celulaQuadrado = quadrado.parentElement;
         if (celulaPersonagem === celulaQuadrado) {
             colidiu = true;
-            let pontosGanhos = 10; // Quantidade de pontos ganhos ao colidir com qualquer quadrado
-            atualizarPontuacao(pontosGanhos); // Chama a função para atualizar a pontuação
-            spawnQuadrados(); // Respawn dos quadrados
+            let pontosGanhos = 10; 
+            atualizarPontuacao(pontosGanhos); 
+            spawnQuadrados();
         }
     });
 
-    if (!colidiu) {
-        // Lógica para quando não há colisão
-    }
+   
 }
 spawnQuadrados();
 setInterval(verificarColisao, 250);
-
-var moedaAdicionada = false; // Variável para rastrear se a moeda foi adicionada
-var slots = document.querySelectorAll('.slot'); // Obtém todos os elementos com a classe 'slot'
-
+//#endregion
+var moedaAdicionada = false; 
+var slots = document.querySelectorAll('.slot'); 
   
-  
+ //#region inventário kinda 
 function adicionarMoeda() {
-    if (pontuacao > 100 && !moedaAdicionada) {
-      // Encontrar o próximo slot disponível
+    if (pontuacao >= 100 && !moedaAdicionada) {
       var proximoSlot = Array.from(slots).find(slot => !slot.classList.contains('ocupado'));
   
       if (proximoSlot) {
-        // Criar um novo elemento moeda
         var moeda = document.createElement('div');
         moeda.classList.add('moeda');
   
-        // Calcular as coordenadas para posicionar a moeda no centro do slot
         var slotRect = proximoSlot.getBoundingClientRect();
-        var moedaSize = 50; // Tamanho da moeda (largura e altura)
+        var moedaSize = 50; 
         var left = slotRect.left + (slotRect.width - moedaSize) / 2;
         var top = slotRect.top + (slotRect.height - moedaSize) / 2;
   
-        // Configurar as coordenadas de posição da moeda
         moeda.style.position = 'absolute';
         moeda.style.left = left + 'px';
         moeda.style.top = top + 'px';
   
-        // Adicionar a moeda ao corpo do documento
         document.body.appendChild(moeda);
   
-        proximoSlot.classList.add('ocupado'); // Marcar o slot como ocupado
-        moedaAdicionada = true; // Atualizar a variável para indicar que a moeda foi adicionada
+        proximoSlot.classList.add('ocupado'); 
+        moedaAdicionada = true; 
   
-        // Resetar a variável moedaAdicionada para permitir adicionar mais moedas
+       
         setTimeout(function() {
           moedaAdicionada = false;
-        }, 1000); // Aguardar 1 segundo (1000 milissegundos) para resetar a variável moedaAdicionada
-  
-        // Subtrair 100 pontos da pontuação
+        }, 1000); 
+        
         pontuacao -= 100;
-        console.log('Pontuação atual: ' + pontuacao); // Mostrar a pontuação atual no console (opcional)
       }
     }
   }
+  //#endregion
+
+  let estado = 'Iniciar';
+  document.getElementById('estado').textContent = estado;
   
+  //#region Estados
+  function AtualizarEstado() {
+    
+  
+    if (emPausa === true) {
+      estado = 'Pausa';
+    } else if (tempoAtual === 0) {
+      estado = 'Fim';
+    } else if (emPausa === false && tempoAtual !== 0) {
+      estado = 'Joga';
+    }
+    document.getElementById('estado').textContent = estado;
+  }
+  
+  setInterval(AtualizarEstado, 250);
+  //#endregion
